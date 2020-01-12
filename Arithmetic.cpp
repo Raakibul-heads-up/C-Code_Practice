35e07db5736abb334e1d64025cde4cfdb43b2d02
Rakibul Islam <58781776+Raakibul-heads-up@users.noreply.github.com>
2020-01-12 19:27:41 +0600

    Add files via upload

---
+++ Arithmetic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout << "Enter two number :";
+    cin >> n1;
+    cin >> n2;
+
+    //int add = n1 +n2;
+    cout << "Addition = " << n1 + n2 << endl;
+    cout << "Subtraction = "<< n1-n2 << endl;
+    cout << "Multiplication = " << n1*n2<<endl;
+    cout << "Division = " << (float)n1/n2 << endl;
+    cout << "Modulus = " << n1%n2<<endl;
+
+return 0;
+}
